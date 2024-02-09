dde35f548d4c440faecdf8b24fd410e99f0d847b
tobySolutions <adedejitobiloba7@gmail.com>
2024-02-09 18:08:46 +0100

    fix: swap phone number input for username

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "flagsapi.com",
+      "userprofile-streamfiles.s3.us-east-2.amazonaws.com",
+      "streaming-app-media-bucket.s3.eu-north-1.amazonaws.com",
+    ],
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,103 @@
-import { CustomHead } from "@/components/custom-head/custom-head"
+import { CustomHead } from "@/components/custom-head/custom-head";
 // import { BaseLayout } from "@/components/layout/base-layout";
 
-import { Button } from "@/components/ui/button"
-import { ButtonWrapper } from "@/components/ui/button-wrapper"
-import { FormField } from "@/components/ui/form-field"
-import { FormGrid } from "@/components/ui/form-grid"
-import { Input } from "@/components/ui/input"
-import { LinkWrapper } from "@/components/ui/link-wrapper"
+import { Button } from "@/components/ui/button";
+import { ButtonWrapper } from "@/components/ui/button-wrapper";
+import { FormField } from "@/components/ui/form-field";
+import { FormGrid } from "@/components/ui/form-grid";
+import { Input } from "@/components/ui/input";
+import { LinkWrapper } from "@/components/ui/link-wrapper";
 import {
   ToastContainerWrapper,
   useToast,
-} from "@/components/ui/toaster-wrapper"
-import { useAuthContext } from "@/context/auth"
+} from "@/components/ui/toaster-wrapper";
+import { useAuthContext } from "@/context/auth";
 // import { CheckMomoUserData, User } from "@/types";
-import { getErrorResponse } from "@/utils"
-import { instance } from "@/utils/instance"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useMutation } from "@tanstack/react-query"
-import { Metadata } from "next"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import { Fragment, useState } from "react"
-import { SubmitHandler, useForm } from "react-hook-form"
-import { z } from "zod"
-import { NextPageWithLayout } from "./_app"
-import { AccountSection } from "@/components/account-section"
-import { BaseLayout } from "@/components/layout/base-layout"
+import {
+  CountryInputProps,
+  getCountries,
+  getErrorResponse,
+  onlyNumberKey,
+} from "@/utils";
+import { instance } from "@/utils/instance";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { Metadata } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Fragment, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { NextPageWithLayout } from "./_app";
+import { AccountSection } from "@/components/account-section";
+import { BaseLayout } from "@/components/layout/base-layout";
+import { SelectField } from "@/components/ui/select-field";
+import { isPossiblePhoneNumber } from "libphonenumber-js";
 
 export interface LoginMutResponse {
-  status: string
-  data: Data
-  message: string
+  status: string;
+  data: Data;
+  message: string;
 }
 
 export interface Data {
-  tokens: Tokens
+  tokens: Tokens;
   // user: User;
 }
 
 export interface Tokens {
-  accessToken: string
-  refreshToken: string
-}
-
-const formSchema = z.object({
-  username: z.string().min(1, "Username is required"),
-  emailAddress: z
-    .string()
-    .email("Invalid email address")
-    .min(1, "Email is required"),
-})
-
-type FormState = z.infer<typeof formSchema>
-export type LoginParams = FormState & {
-  isMomoUser?: string
-  momoMssidn?: string
+  accessToken: string;
+  refreshToken: string;
 }
 
 export const metadata: Metadata = {
   title: "Login to Your Account",
-}
+};
 
 export default function Home() {
-  const { asPath } = useRouter()
+  const { asPath } = useRouter();
+  const [countryCode, setCountryCode] = useState<CountryInputProps>();
+  const [phoneNumber, setPhoneNumber] = useState("");
   // const { setAuthState } = useAuthContext();
+  type FormState = z.infer<typeof formSchema>;
+
+  const formSchema = z.object({
+    phoneNumber: z.string().refine(
+      () => {
+        const isAValidPhoneNumber = isPossiblePhoneNumber(
+          phoneNumber ?? "",
+          countryCode?.countryName ?? "NG"
+        );
+
+        return isAValidPhoneNumber;
+      },
+      {
+        message: "Invalid Phone Number",
+      }
+    ),
+    emailAddress: z
+      .string()
+      .email("Invalid email address")
+      .min(1, "Email is required"),
+  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    trigger,
   } = useForm<FormState>({
     resolver: zodResolver(formSchema),
-  })
+  });
 
-  const { showToast } = useToast()
-  const { mutate, isLoading } = useMutation(async (variables: LoginParams) => {
-    const { data } = await instance.post<LoginMutResponse>(
-      "/user-auth/login",
-      variables
-    )
-    return data
-  })
+  const { showToast } = useToast();
+  // const { mutate, isLoading } = useMutation(async (variables: LoginParams) => {
+  //   const { data } = await instance.post<LoginMutResponse>(
+  //     "/user-auth/login",
+  //     variables
+  //   );
+  //   return data;
+  // });
 
   // const onSubmit: SubmitHandler<FormState> = ({ emailAddress, password }) => {
   //   const { success: isEmail } = z.string().email().safeParse(emailAddress);
@@ -149,17 +166,35 @@ export default function Home() {
           >
             <FormGrid offset="intermediate">
               <FormGrid.Container>
-                <FormGrid.Tile>
-                  <FormField id="password" label="Username">
+                <FormGrid.Tile size="thirty">
+                  <SelectField
+                    options={getCountries()}
+                    id="country_code"
+                    label="Country Code"
+                    placeholder="Search"
+                    size="md"
+                    handleChange={(value: CountryInputProps) =>
+                      setCountryCode(value)
+                    }
+                    isSearchable
+                  />
+                </FormGrid.Tile>
+                <FormGrid.Tile size="seventy">
+                  <FormField id="phone" label="Phone Number ">
                     <Input
-                      type="text"
-                      placeholder="Enter your username"
-                      {...register("username")}
-                      error={Boolean(errors.username)}
-                      errorMsg={
-                        // errors?.username?.message
-                        "Stop"
-                      }
+                      {...register("phoneNumber", {
+                        onChange: (e) => {
+                          setPhoneNumber(e.target.value);
+                        },
+                      })}
+                      placeholder="Enter phone number"
+                      onKeyDown={onlyNumberKey}
+                      maxLength={11}
+                      error={Boolean(errors.phoneNumber)}
+                      onBlur={() => {
+                        trigger("phoneNumber");
+                      }}
+                      errorMsg={errors?.phoneNumber?.message}
                     />
                   </FormField>
                 </FormGrid.Tile>
@@ -220,7 +255,7 @@ export default function Home() {
                 type="submit"
                 variant={"alternate3"}
                 width={"full"}
-                isLoading={isLoading}
+                // isLoading={isLoading}
               >
                 Continue
               </Button>
@@ -230,5 +265,5 @@ export default function Home() {
         </AccountSection>
       </Fragment>
     </BaseLayout>
-  )
+  );
 }
